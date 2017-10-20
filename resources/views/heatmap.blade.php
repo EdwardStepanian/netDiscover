e872ef6db6b9e6917a6c72cf4839bf2b2cbe302b
EdwardStepanian <edward.stepanian.se@gmail.com>
2017-10-20 17:28:25 +0400

    initial map

---
+++ resources/views/heatmap.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div id="map-canvas" style="width: 500px; height: 100px"></div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/heatmap', 'MapController@show')->name('heatmap');
