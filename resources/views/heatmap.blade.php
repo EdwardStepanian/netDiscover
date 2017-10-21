f4d407a1c043541dbb4fd720cb1531f449931a94
EdwardStepanian <edward.stepanian.se@gmail.com>
2017-10-21 04:27:42 +0400

    HeatMap

--- resources/views/heatmap.blade.php
+++ resources/views/heatmap.blade.php
@@ -3,11 +3,13 @@
 
 @section('content')
             <div id="map-canvas"></div>
-
-    <script src="https://maps.googleapis.com/maps/api/js?sensor=false"
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB72qv_GuZVo7oBUUOGc_APkbRGGA4iHlc&sensor=false"
             async defer></script>
     <script src="{{ asset('js/buildHeatmap.js') }}"></script>
     <script src="{{ asset('js/gmaps-heatmap.js') }}"></script>
-    <script src="{{ asset('js/heatMap.js') }}"></script>
+
+            <script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.devbridge-autocomplete/1.4.3/jquery.autocomplete.min.js"> </script>
+            <script src="{{ asset('js/heatMap.js') }}"></script>
 @endsection
 
